7af86acbdc60520705cb02219b0f08911c84e55f
Thomas Fowler <thomas.fowler@flatironschool.com>
2021-09-23 18:10:19 -0600

    Modified solution.

--- javascript/rotate_array.js
+++ javascript/rotate_array.js
@@ -1,21 +1,36 @@
-function rotateArray(arr, k) {
-  // type your code here
+const rotateArray = (arr, k) => {
+  const length = arr.length
+  return arr.reduce((p, c, i) => {
+    const steps =  k >= length ? (k % length) + i : k + i
+    const newIndex = steps >= length ? steps - length : steps
+    p[newIndex] = c
+    return p
+  }, [])
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: [4, 1, 2, 3]");
-  console.log("=>", rotateArray([1, 2, 3, 4], 1));
+  // console.log("Given: [1, 2, 3, 4], 1");
+  // console.log("Expecting: [4, 1, 2, 3]");
+  // console.log("=>", rotateArray([1, 2, 3, 4], 1));
+
+  // console.log("");
+
+  // console.log("Given: [1, 2, 3], 2");
+  // console.log("Expecting: [2, 3, 1]");
+  // console.log("=>", rotateArray([1, 2, 3], 2));
 
   console.log("");
 
+  console.log("Given: [1, 2, 3], 5");
   console.log("Expecting: [2, 3, 1]");
-  console.log("=>", rotateArray([1, 2, 3], 2));
+  console.log("=>", rotateArray([1, 2, 3], 5));
 
-  console.log("");
+  // console.log("");
 
-  console.log("Expecting: [1, 2, 3]");
-  console.log("=>", rotateArray([1, 2, 3], 3));
+  // console.log("Expecting: [1, 2, 3], 3");
+  // console.log("Expecting: [1, 2, 3]");
+  // console.log("=>", rotateArray([1, 2, 3], 3));
 }
 
 module.exports = rotateArray;
